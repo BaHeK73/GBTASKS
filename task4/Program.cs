f6a623f54522651bf893fa837ea941a3deed4132
BaHeK73 <BaHeK73>
2022-12-20 00:24:55 +0400

    I wrote a program for task 4

--- task4/Program.cs
+++ task4/Program.cs
@@ -5,3 +5,21 @@
 //22 3 9 -> 22
 
 using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine());
+
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine());
+
+WriteLine("Введите третье число: ");
+int c = int.Parse(ReadLine());
+
+int max = 0;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Write("max = ");
+WriteLine(max);
