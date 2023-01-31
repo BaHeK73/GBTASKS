85a312b921493ed1b1d7d9ffc242ff668c6e08dd
BaHeK73 <BaHeK73>
2023-01-31 10:26:36 +0400

    I wrote a program for task 64

---
+++ task64/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 64: Напишите программу, которая реализует рекурсивный метод вывода всех натуральных числел в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using static System.Console;
+Clear();
+
+int n = InputInt("Введите положительное число: ");
+int m = 1;
+if (n < 1)
+{
+    WriteLine("Ввели не положительное число");
+}
+WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Write(output);
+    return int.Parse(ReadLine()!);
+}
+
